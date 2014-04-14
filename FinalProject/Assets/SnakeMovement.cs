07b566d72ff994cc000b9f5f1f37490db5828a42
Josh Sarath <joshua.sarath@gmail.com>
2014-04-14 00:38:45 -0400

    colliders working

--- FinalProject/Assets/SnakeMovement.cs
+++ FinalProject/Assets/SnakeMovement.cs
@@ -6,7 +6,9 @@ public class SnakeMovement : MonoBehaviour {
 	private float xspeed;
 	private float yspeed;
 	public float moveunit;//constant unit to adjust speed
-
+	public GameObject snakeSegment;
+	private float counter;
+	public float timedelay;
 
 	// Use this for initialization
 	void Start () {
@@ -15,39 +17,59 @@ public class SnakeMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("up")) {
-			yspeed=moveunit;
-			transform.position+=new Vector3(0,yspeed,0);
-			xspeed=0;
-			yspeed=0;
+		if (counter<=0){
+			if (Input.GetKey ("up")) {
+				yspeed=moveunit;
+				transform.position+=new Vector3(0,yspeed,0);
+				xspeed=0;
+				yspeed=0;
+				counter=timedelay;
+			}
+			if (Input.GetKey ("down")) {
+				yspeed=moveunit;
+				transform.position-=new Vector3(0,yspeed,0);
+				xspeed=0;
+				yspeed=0;
+				counter=timedelay;
+			}
+			if (Input.GetKey ("left")) {
+				xspeed=moveunit;
+				transform.position-=new Vector3(xspeed,0,0);
+				xspeed=0;
+				yspeed=0;
+				counter=timedelay;
+			}
+			if (Input.GetKey ("right")) {
+				xspeed=moveunit;//sets speed to constant unit
+				transform.position+=new Vector3(xspeed,0,0);//changes position on new 1 directional vector3
+				xspeed=0;//resets speed to 0 so after key lifted no more movement
+				yspeed=0;
+				counter=timedelay;
+			}
+
 		}
-		if (Input.GetKey ("down")) {
-			yspeed=moveunit;
-			transform.position-=new Vector3(0,yspeed,0);
-			xspeed=0;
-			yspeed=0;
-		}
-		if (Input.GetKey ("left")) {
-			xspeed=moveunit;
-			transform.position-=new Vector3(xspeed,0,0);
-			xspeed=0;
-			yspeed=0;
-		}
-		if (Input.GetKey ("right")) {
-			xspeed=moveunit;//sets speed to constant unit
-			transform.position+=new Vector3(xspeed,0,0);//changes position on new 1 directional vector3
-			xspeed=0;//resets speed to 0 so after key lifted no more movement
-			yspeed=0;
-		}
+		counter-=Time.deltaTime;
+		//Debug.Log (counter);
 	}
 	void OnTriggerEnter(Collider other){
-		/*
-		if (other.name == "Cube") {
-			Debug.Log ("poop");
-			transform.position+=new Vector3(0,0,0);
+
+		if (other.name == "Food(Clone)") {
+			//Debug.Log ("poop");
+			Destroy(GameObject.Find (other.name));
+			Instantiate(snakeSegment);
 			//transform.position=new Vector3((other.transform.position.x-.5f), transform.position.y,0);  
 			//Destroy (gameObject);
 		}
-*/
+		if (other.name=="Segment(Clone)"){
+			Debug.Log ("hit");
+			Destroy (GameObject.Find (this.name));	
+		}
+		if (other.name == "Wall(Clone)") {
+			/* stop movememt entirely 
+			 * or end game if too difficult
+			 * 
+			 */
+		}
+
 	}
 }
--- FinalProject/Assets/StartSnake.cs
+++ FinalProject/Assets/StartSnake.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class StartSnake : MonoBehaviour {
 	public GameObject snake;
+	public GameObject food;
 	// Use this for initialization
 	void Start () {
 		Instantiate (snake);
+		Instantiate (food);
+		Instantiate (food);
+
+
 	}
 	
 	// Update is called once per frame
