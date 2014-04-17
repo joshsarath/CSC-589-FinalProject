2a7b46d3bfbe7dad49803a6ae63e1a11206e2259
Josh Sarath <joshua.sarath@gmail.com>
2014-04-17 00:35:19 -0400

    follow started

--- FinalProject/Assets/Follow.cs
+++ FinalProject/Assets/Follow.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Follow : MonoBehaviour {
 	public GameObject toFollow;
+	public bool followed=false;
+	public Vector3 lastposition;
 	// Use this for initialization
 	void Start () {
-		
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("Snake")) {
+			Follow segment= GameObject.Find (go.name).GetComponent<Follow>();
+			if (segment.followed==false){
+				toFollow=segment.gameObject;
+			}		
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		lastposition = transform.position;
 		transform.position = toFollow.transform.position;
 	}
 }
