17bd830ddd0f9d63158c0170c6f815ca5d990055
Josh Sarath <joshua.sarath@gmail.com>
2014-04-19 20:10:09 -0400

    follow

--- FinalProject/Assets/Follow.cs
+++ FinalProject/Assets/Follow.cs
@@ -2,29 +2,26 @@
 using System.Collections;
 
 public class Follow : MonoBehaviour {
-	public bool followed;
 	public Vector3 lastposition;
-	private GameObject toFollow;
+	public Follow piece;
 	// Use this for initialization
 	void Start () {
-		//foreach (GameObject go in GameObject.FindGameObjectsWithTag("Snake")) {
-			//Debug.Log ("count");
-			Follow segment= GameObject.Find ("Segment(Clone)").GetComponent<Follow>();
-			if (segment.followed==false){
-				toFollow=GameObject.Find(segment.name);
-			}		
-		//}
+		if (this.name != "Head(Clone)") {
+			piece= GameObject.Find("Head(Clone)").GetComponent<Follow>();
+
+		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*lastposition =new Vector3(transform.position.x,transform.position.y,transform.position.z);
-		//Debug.Log (toFollow.name);
-		toFollow.transform.position = this.lastposition;
-		if (Input.GetKey("space")){
-			Debug.Log (toFollow.name);
-		}*/
+		if (lastposition != transform.position) {
+			lastposition = transform.position;
+		}
+		if (this.name != "Head(Clone)") {
+			transform.position= piece.lastposition;	
+			Debug.Log(lastposition);
+		}
 
 	}
 }
