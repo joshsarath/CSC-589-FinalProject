ccf3ae8c96a7c6d7a914872b77b7abcc65618a49
Josh Sarath <joshua.sarath@gmail.com>
2014-04-23 21:16:57 -0400

    Revert 39829b1..32adf3e
    This rolls back to commit 39829b11469f99efc8ba98e9b9db0ee0c9145b74.

--- FinalProject/Assets/Follow.cs
+++ FinalProject/Assets/Follow.cs
@@ -2,9 +2,47 @@
 using System.Collections;
 
 public class Follow : MonoBehaviour {
+	public Vector3 lastposition;
+	public Vector3 followposition;
+	public bool followed= false;
+	// Use this for initialization
+	void Start () {
+		if (this.name != "Head(Clone)") {
+			Follow piece= GameObject.Find ("Head(Clone)").GetComponent<Follow>();
+			piece.followed=true;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.name == "Head(Clone)") {
+			if (lastposition != transform.position) {
+				lastposition=transform.position;
+				followposition=lastposition;
+			}
+		}
+		if (this.name!="Head(Clone)"){
+			if (followposition != transform.position) {
+				lastposition = transform.position;
+				followposition=lastposition;
+			}
+			Follow piece= GameObject.Find ("Head(Clone)").GetComponent<Follow>();
+			transform.position=piece.followposition;
+		}
+		if (Input.GetKey("space")){
+			Debug.Log(lastposition+", "+followposition);
+		}
+
+	}
+}
+/*using System.Collections;
+
+public class Follow : MonoBehaviour {
 	public bool followed;
 	public Vector3 lastposition;
 	private GameObject toFollow;
+	public Follow piece;
 	// Use this for initialization
 	void Start () {
 		//foreach (GameObject go in GameObject.FindGameObjectsWithTag("Snake")) {
@@ -14,17 +52,29 @@ public class Follow : MonoBehaviour {
 				toFollow=GameObject.Find(segment.name);
 			}		
 		//}
+		if (this.name != "Head(Clone)") {
+			piece= GameObject.Find("Head(Clone)").GetComponent<Follow>();
+
+		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*lastposition =new Vector3(transform.position.x,transform.position.y,transform.position.z);
+		lastposition =new Vector3(transform.position.x,transform.position.y,transform.position.z);
 		//Debug.Log (toFollow.name);
 		toFollow.transform.position = this.lastposition;
 		if (Input.GetKey("space")){
 			Debug.Log (toFollow.name);
-		}*/
-
-	}
+		}
+if (lastposition != transform.position) {
+	lastposition = transform.position;
 }
+if (this.name != "Head(Clone)") {
+	transform.position= piece.lastposition;	
+	Debug.Log(lastposition);
+}
+
+}
+}
+*/
