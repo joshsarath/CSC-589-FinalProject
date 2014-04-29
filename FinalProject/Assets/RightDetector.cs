18da41bc21db9c436bd9eda6b3d1877d88f86bb8
joshsarath <joshua.sarath@gmail.com>
2014-04-28 20:21:58 -0400

    Revert 26d68cc..8ed746f
    This rolls back to commit 26d68cc794e1f66539233925a2f31f7ac4eb5507.

---
+++ FinalProject/Assets/RightDetector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightDetector : MonoBehaviour {
+	public bool move=false;//will allow head to move in that direction
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.name == "Wall(Clone)"){
+			move=true;
+		}
+		if (other.name == "Segment(Clone)") {
+			move=true;
+		}
+	}
+	void OnTriggerExit(){
+		move = false;
+	}
+}
