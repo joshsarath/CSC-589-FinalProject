8ed746f9a3fd79661ed3bed80fdf0b2f3de37fde
joshsarath <joshua.sarath@gmail.com>
2014-04-28 20:21:40 -0400

    small changes

--- FinalProject/Assets/StartSnake.cs
+++ FinalProject/Assets/StartSnake.cs
@@ -27,14 +27,14 @@ public class StartSnake : MonoBehaviour {
 
 	}
 	void buildWalls( int size){
-		GameObject top = Instantiate (wall, new Vector3 (0, (arena/2), 0), Quaternion.identity) as GameObject;
-		top.transform.localScale = new Vector3 (20,.5f,1);
-		GameObject bottom = Instantiate (wall, new Vector3 (0, -(arena/2), 0), Quaternion.identity) as GameObject;
-		bottom.transform.localScale = new Vector3 (20,.5f,1);
-		GameObject right = Instantiate (wall, new Vector3 ((arena/2), 0, 0), Quaternion.identity) as GameObject;
-		right.transform.localScale = new Vector3 (.5f,20,1);
-		GameObject left = Instantiate (wall, new Vector3 (-(arena/2), 0, 0), Quaternion.identity) as GameObject;
-		left.transform.localScale = new Vector3 (.5f,20,1);
+		GameObject top = Instantiate (wall, new Vector3 (0, (arena/2)+1, 0), Quaternion.identity) as GameObject;
+		top.transform.localScale = new Vector3 (22,.5f,1);
+		GameObject bottom = Instantiate (wall, new Vector3 (0, -(arena/2)-1, 0), Quaternion.identity) as GameObject;
+		bottom.transform.localScale = new Vector3 (22,.5f,1);
+		GameObject right = Instantiate (wall, new Vector3 ((arena/2)+1, 0, 0), Quaternion.identity) as GameObject;
+		right.transform.localScale = new Vector3 (.5f,22,1);
+		GameObject left = Instantiate (wall, new Vector3 (-(arena/2)-1, 0, 0), Quaternion.identity) as GameObject;
+		left.transform.localScale = new Vector3 (.5f,22,1);
 		for (int i=0; i<arena; i++) {
 			Vector3 position=new Vector3( Random.Range(-arena/2, arena/2), Random.Range(-arena/2, arena/2),0);
 			Instantiate (wall, position, Quaternion.identity);
